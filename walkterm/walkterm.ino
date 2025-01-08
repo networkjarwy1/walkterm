e50f41a47dc3b7f5fb182017b25a273d9d3f262a
Jaroslav Vizner <networkjarwy@gmail.com>
2025-01-08 14:59:20 +0100

    update README.md & adding command execution in terminal of walkterm

--- walkterm/walkterm.ino
+++ walkterm/walkterm.ino
@@ -1,4 +1,16 @@
 #include <TFT_eSPI.h>
+#include <Arduino.h>
+
+char charTable[] = {
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    '/', '*', '\'', '!', '@', '#', '$', '%', '^', '&', '(', ')', '-', 
+    '_', '=', '+', '[', ']', '{', '}', ';', ':', '"', '<', '>', ',', 
+    '.', '?', '\\', '|', '`', '~', '0', '1', '2', '3', '4', '5', '6', 
+    '7', '8', '9', '\n', '\r'
+};
 
 #define CS 0
 #define DATA 1
@@ -21,7 +33,6 @@ void setup() {
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_GREEN);
   tft.setCursor(0, 0);
-  tft.println("Waiting for input...");
 }
 
 void loop() {
@@ -35,8 +46,8 @@ void loop() {
   }
 }
 
-byte getKbInput(){
-  byte currentByte = 0;
+char getKbInput(){
+  unit8_t currentByte = 0;
   int bitCount = 0;
   
   bool lastCLKstate = LOW;
@@ -52,5 +63,47 @@ byte getKbInput(){
     }
   }
 
-  return currentByte;
+  return decode(currentByte);
+}
+
+char decode(unit8_t hex){
+  return charTable[hex];
+}
+
+void execCommand(char command[]) {
+  char* token = strtok(command, " ");
+
+  if (token == NULL) {
+    return;
+  }
+
+  if (strcmp(token, "connect") == 0) {
+    token = strtok(NULL, " ");
+    
+    if (token == NULL) {
+      tft.println("you forgot to select device \n valid format: connect <device> wifi <SSID> <password>");
+      return;
+    }
+    char* device = token;
+
+    token = strtok(NULL, " ");
+    if (token == NULL || strcmp(token, "wifi") != 0) {
+      tft.println("you forgot to select type of connection \n valid format: connect <device> wifi <SSID> <password>");
+      return;
+    }
+    
+    token = strtok(NULL, " ");
+    if (token == NULL) {
+      tft.println("you forgot to select SSID \n valid format: connect <device> wifi <SSID> <password>");
+      return;
+    }
+    char* ssid = token;
+
+    token = strtok(NULL, " ");
+    if (token == NULL) {
+      tft.println("you forgot to select password \n valid format: connect <device> wifi <SSID> <password>");
+      return;
+    }
+    char* password = token;
+  }
 }
